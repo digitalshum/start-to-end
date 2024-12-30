41962b6fabdb45f406c2e863831e7b549b981473
digitalshum <digitalshum@gmail.com>
2024-12-30 21:56:40 +0500

    java

---
+++ JavaScript/Assignment One/app.js
@@ -0,0 +1,46 @@
+// document.write('Welcome to \"JavaScript\".');
+alert("Hello! I am an alert box!!");
+alert("Thank You!");
+alert("Hello\nHow are you?");
+var first_name = 'Shumaila';
+var last_name = 'Qamar';
+alert(first_name + " " + last_name);
+var age = 18;
+alert('Your age is ' + age);
+var my_name = 'Shum';
+if (my_name == 'Shum') {
+    alert('Hi Miss. Shum');
+}
+var nationality;
+alert(nationality = "Pakistani.");
+let person = prompt("Please enter your name", "Digital Shum");
+
+
+let text;
+let favDrink = prompt("What's your favorite cocktail drink?");
+switch (favDrink) {
+    case "Coca-Cola":
+        text = "Excellent choice! Coca-Cola is good for your soul.";
+        break;
+    case "Pepsi":
+        text = "Pepsi is my favorite too!";
+        break;
+    case "Sprite":
+        text = "Really? Are you sure the Sprite is your favorite?";
+        break;
+    default:
+        text = "I have never heard of that one!";
+}
+
+var age = 20;
+var sum = age + 2;
+alert('age + 2=' + sum);
+
+var a = 5;
+var b = "6"
+var c = a + b;
+alert(c);
+
+var reminder = 10 % 3
+alert(reminder);
+
